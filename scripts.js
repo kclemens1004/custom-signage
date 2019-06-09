70fd96a0b32a2f239985f6c704e95d0fbfbc470e
kclemens1004 <kennethwclemens@gmail.com>
2019-06-09 13:08:30 -0400

    added min value, reset functionality and restyled results

--- scripts.js
+++ scripts.js
@@ -1,11 +1,48 @@
-function calculateCost() {
-    var titleText = document.getElementById('title-text');
-    var customSignage = document.getElementById('custom-signage');
-    var cost = customSignage.value.length * 5;
-    titleText.innerText = "$ " + cost;
-    console.log(cost);
+var actionButton = document.getElementById('action-button');
+var error = document.getElementById('error');
+var field = document.getElementById('field');
+var resultText = document.getElementById('result-text');
+var resultPrice = document.getElementById('result-price');
+var resultBlock = document.getElementById('result');
+var customSignage = document.getElementById('custom-signage');
+
+var priced = false;
+
+function clickControl () {
+    if (priced === false) {
+        calculateCost();
+    }
+
+    else {
+        priced = false;
+        resultBlock.style.display = "none";
+        field.style.display = "";
+        actionButton.innerText = "SHOW COST";
+        customSignage.value = "";
+    }
 }
 
-var actionButton = document.getElementById('action-button');
-actionButton.onclick = calculateCost;
+function calculateCost() {
+
+    if (customSignage.value.length > 4) {
+    
+        var cost = customSignage.value.length * 5;
+
+        resultText.innerText = customSignage.value;
+        resultPrice.innerText = "$" + cost;
+        error.innerText = "";
+        field.style.display = "none";
+        resultBlock.style.display = "flex";
+        actionButton.innerText = "START OVER";
+        priced = true;
+    }
+
+    else {
+        error.innerText = "5 character minimum!";
+    }
+
+}
+
+
+actionButton.onclick = clickControl;
 
